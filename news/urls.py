53c8ff8aac89419d3492f071c94c1afb4a41d617
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-09-03 09:17:37 +0300

    initial commit

--- news/urls.py
+++ news/urls.py
@@ -4,10 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    # path('',views.welcome,name = 'welcome'),
     path('',views.news_today,name='newsToday'),
     path('archives/<past_date>/',views.past_days_news,name = 'pastNews'),
-    path('search/', views.search_results, name='search_results')
-]
+    path('search/', views.search_results, name='search_results'),
+    path('article/<int:article_id>/',views.article, name= 'article')]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
